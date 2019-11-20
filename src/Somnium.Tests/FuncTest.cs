5c3dc2628d6c6845964d75695dd9a6a1465cda0b
xin-pu <pu.xin@outlook.com>
2019-11-20 20:26:53 +0800

    Create Project and Activate Func

---
+++ src/Somnium.Tests/FuncTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Somnium.Func;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Somnium.Tests
+{
+    [TestClass]
+    public class FuncTest
+    {
+        [TestMethod]
+        public void ActivationTest()
+        {
+            var activations = new List<Func<double, double>> { Activation.sigmoid, Activation.tanh, Activation.max };
+            var res = activations.Select(a => a(0)).ToArray();
+            Assert.AreEqual(0.5, res[0], 1E-4);
+            Assert.AreEqual(0, res[1], 1E-4);
+            Assert.AreEqual(0, res[2], 1E-4);
+        }
+    }
+}
