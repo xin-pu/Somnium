f33298ad3ceca1fc106a7b9379da4aca26042e88
xin-pu <pu.xin@outlook.com>
2019-11-21 22:07:17 +0800

    Add Optimizer class

---
+++ src/Somnium/Func/Optimizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Somnium.Func
+{
+    public class Optimizer
+    {
+    }
+}
